6a9889371df4f824db92411530498fdd31fc2e97
Miroslav Kravec <kravec.miroslav@gmail.com>
2020-02-21 19:17:14 +0100

    Extract tested config to TOML file

--- generator_test.go
+++ generator_test.go
@@ -2,6 +2,9 @@ package mikrogen
 
 import (
 	"fmt"
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/toml"
+	"github.com/knadh/koanf/providers/file"
 	"gotest.tools/assert"
 	"io/ioutil"
 	"path"
@@ -17,44 +20,8 @@ func TestGenerate(t *testing.T) {
 	}{
 		{
 			"01",
-			Configuration{
-				IdentifierPrefix: "BlockWeb",
-
-				DNSBlockedAddresses: []string{
-					"facebook.com",
-					"static.facebook.com",
-					"www.facebook.com",
-					"api.facebook.com",
-					"edge-mqtt.facebook.com",
-					"messenger.com",
-					"www.messenger.com",
-				},
-
-				TLSBlockedAddresses: []string{
-					"facebook.com",
-					"*.facebook.com",
-					"messenger.com",
-					"*.messenger.com",
-					"fbcdn.net",
-					"*.fbcdn.net",
-				},
-
-				DisableIntervals: []Interval{
-					{
-						"08:30:00",
-						"09:08:00",
-					},
-					{
-						"14:00:00",
-						"15:05:00",
-					},
-					{
-						"18:00:00",
-						"19:03:00",
-					},
-				},
-			},
-			loadFile("output_01.txt"),
+			loadConfig("sample_01.in.toml"),
+			loadFile("sample_01.out"),
 		},
 	}
 	for _, tt := range tests {
@@ -66,13 +33,33 @@ func TestGenerate(t *testing.T) {
 	}
 }
 
+func loadConfig(filename string) Configuration {
+	k := koanf.New(".")
+	err := k.Load(file.Provider(getLocalFile(filename)), toml.Parser())
+	if err != nil {
+		panic(err)
+	}
+
+	var c Configuration
+	err = k.Unmarshal("", &c)
+	if err != nil {
+		panic(err)
+	}
+	return c
+}
+
 func loadFile(name string) string {
-	_, currentFile, _, _ := runtime.Caller(0)
-	dir := path.Dir(currentFile)
-	filepath := path.Join(dir, name)
+	filepath := getLocalFile(name)
 	result, err := ioutil.ReadFile(filepath)
 	if err != nil {
 		panic(fmt.Errorf("failed to load %s: %w", filepath, err))
 	}
 	return string(result)
+}
+
+func getLocalFile(name string) string {
+	_, currentFile, _, _ := runtime.Caller(0)
+	dir := path.Dir(currentFile)
+	filepath := path.Join(dir, name)
+	return filepath
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/google/go-cmp v0.4.0 // indirect
+	github.com/knadh/koanf v0.8.1
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/tools v0.0.0-20200221152158-fe62aff31966 // indirect
 	gotest.tools v2.2.0+incompatible
